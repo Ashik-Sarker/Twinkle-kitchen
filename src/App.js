0133537e4ee8ddd3418d73635801c40fe63a9d56
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-04-17 01:12:47 +0600

    react routing and navbar done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Pages/Common/NavBar/NavBar';
+import Footer from './Pages/Common/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import Services from './Pages/Home/Services/Services';
+import Blogs from './Pages/Home/Blogs/Blogs';
+import Login from './Pages/Login/Login/Login';
+import Registration from './Pages/Login/Registration/Registration';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/services/:serviceId' element={<Services></Services>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/registration' element={<Registration></Registration>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Common/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <Navbar className='py-4' collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand as={Link} to='/'>Pro Developer</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#services">Services</Nav.Link>
+                    <Nav.Link href="#blog">Blogs</Nav.Link>
+                    <Nav.Link as={Link} to='/about'>About Us</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={Link} to='/login'>LogIn</Nav.Link>
+                    <Nav.Link eventKey={2} as={Link} to='/registration'>Registration</Nav.Link> 
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
